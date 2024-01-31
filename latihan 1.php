8c3caa75e56c9bb8b8599d4a2571b0a75acbc8e6
Afrizal Dwi Handika <142969775+Gracetav@users.noreply.github.com>
2024-01-31 20:13:56 +0700

    Add files via upload

---
+++ latihan 1.php
@@ -0,0 +1,32 @@
+<?php
+
+$siswa = ["Fahmi","Farhan","Farid","Rama","Naufal","Rio","Chikal"];
+print_r($siswa);
+echo "<br/>";
+
+$kegiatan = ["Sedang Coding","Sedang Desain",];
+
+echo $siswa[1];
+echo $kegiatan[0];
+
+echo "<br/>";
+echo $siswa[4];
+echo $kegiatan[1];
+
+//cara dua
+
+// $siswa = ["Fahmi","Farhan","Farid","Rama","Naufal","Rio","Chikal"];
+// print_r($siswa);
+// echo "<br/>";
+
+// $kegiatan = ["Sedang Coding","Sedang Desain",];
+
+// echo "$siswa[1] Sedang coding";
+
+
+// echo "<br/>";
+// echo "$siswa[4] Sedang desain";
+
+
+
+?>
